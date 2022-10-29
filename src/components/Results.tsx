48395fd0d0e2849393449f709a3b58ef795bb385
steve <steve@steve.computer>
2022-10-29 11:52:46 -0500

    tabs

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -24,47 +24,46 @@ export default function Results() {
     const $appStore = useStore(appStore);
 
     return <div>
-        <Show when={$appStore().results}>
-            <h3>Objects</h3>
-            <For each={Array.from($appStore().results!.objects.entries())}>
-                {([object_type, values]) =>
-                    <div>
-                        <h4>{object_type}</h4>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <For each={Array.from(values[0]!.value.fields!.keys())}>
-                                        {key => <th>{key}</th>}
-                                    </For>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <For each={values}>
-                                    {value => <tr>
-                                        <For each={Array.from(value.value.fields!.values())}>
-                                            {(value) => <td>{display(value)}</td>}
-                                        </For>
-                                    </tr>}
-                                </For>
-                            </tbody>
-                        </table>
-                    </div>
-                }
-            </For>
-            <h3>Output</h3>
-            <For each={Array.from($appStore().results!.output)}>
-                {output => <div>{output.value}</div>}
-            </For>
-            <h3>Errors</h3>
+        <Show when={!($appStore().results)}>
+            <div id="objects-tab" class="tab content1">
+                Run to see results
+            </div>
         </Show>
-
-
-        {/* <div>{JSON.stringify(results.objects)}<div /> */}
-        {/* <div>
-                <h2>Results</h2>
-                <p>{JSON.stringify($appStore().results)}</p>
-                <h2>Errors</h2>
-                <p>{JSON.stringify($appStore().errors)}</p>
-            </div> */}
+        <Show when={$appStore().results}>
+            <div id="objects-tab" class="tab content1">
+                <h3>Objects</h3>
+                <For each={Array.from($appStore().results!.objects.entries())}>
+                    {([object_type, values]) =>
+                        <div>
+                            <h4>{object_type}</h4>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <For each={Array.from(values[0]!.value.fields!.keys())}>
+                                            {key => <th>{key}</th>}
+                                        </For>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <For each={values}>
+                                        {value => <tr>
+                                            <For each={Array.from(value.value.fields!.values())}>
+                                                {(value) => <td>{display(value)}</td>}
+                                            </For>
+                                        </tr>}
+                                    </For>
+                                </tbody>
+                            </table>
+                        </div>
+                    }
+                </For>
+            </div>
+            <div id="output-tab" class="tab content1">
+                <h3>Output</h3>
+                <For each={Array.from($appStore().results!.output)}>
+                    {output => <div>{output.value}</div>}
+                </For>
+            </div>
+        </Show>
     </div>
 }
